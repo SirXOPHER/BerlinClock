abada23e8fe83f44b3db4c1ff495a1d284cb7c0e
SirXOPHER <chris.reinmuller@gmail.com>
2018-10-30 15:32:14 +0000

    Add naive conversion of digital time into berlin clock time.

---
+++ src/test/java/ShowTimeOnBerlinClockFeature.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class ShowTimeOnBerlinClockFeature {
+
+    @Test
+    void should_convert_digital_time_into_berlin_clock_time() {
+        BerlinClock berlinClock = new BerlinClock();
+        String digitalTime = "10:13:00";
+
+
+        String berlinClockTime = berlinClock.convert(digitalTime);
+
+
+        String expectedBerlinClockTime =
+                        "                 * *\n" +
+                        "               *     *\n" +
+                        "              *   Y   *\n" +
+                        "               *     *\n" +
+                        "                 * *\n" +
+                        " ╔═══════╗╔═══════╗╔═══════╗╔═══════╗\n" +
+                        " ║   R   ║║   R   ║║   O   ║║   O   ║\n" +
+                        " ╚═══════╝╚═══════╝╚═══════╝╚═══════╝\n" +
+                        " ╔═══════╗╔═══════╗╔═══════╗╔═══════╗\n" +
+                        " ║   O   ║║   O   ║║   O   ║║   O   ║\n" +
+                        " ╚═══════╝╚═══════╝╚═══════╝╚═══════╝\n" +
+                        " ╔═╗╔═╗╔═╗ ╔═╗╔═╗╔═╗ ╔═╗╔═╗╔═╗ ╔═╗╔═╗\n" +
+                        " ║Y║║Y║║O║ ║O║║O║║O║ ║O║║O║║O║ ║O║║O║\n" +
+                        " ╚═╝╚═╝╚═╝ ╚═╝╚═╝╚═╝ ╚═╝╚═╝╚═╝ ╚═╝╚═╝\n" +
+                        " ╔═══════╗╔═══════╗╔═══════╗╔═══════╗\n" +
+                        " ║   Y   ║║   Y   ║║   Y   ║║   O   ║\n" +
+                        " ╚═══════╝╚═══════╝╚═══════╝╚═══════╝";
+        assertThat(berlinClockTime, is(equalTo(expectedBerlinClockTime)));
+    }
+}
